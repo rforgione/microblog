270bfdfd4cf14cba21b78d2248a3505e7c14ef6e
rforgione <rforgione@gmail.com>
2016-08-13 18:20:40 -0400

    complete part III

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 # importing the views module from the app package requires that the 'app'
 # variable be defined, i.e., there MUST be a variable named app containing a
 # Flask object.
