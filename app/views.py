9c1e01c0ddc9dc188367204fdc82ecae5ccc079c
rforgione <rforgione@gmail.com>
2016-08-13 17:01:13 -0400

    finish part II

--- app/views.py
+++ app/views.py
@@ -5,6 +5,21 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'nickname': 'Rob'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Rob'},
+            'body': 'Can\'t wait for football season!'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
