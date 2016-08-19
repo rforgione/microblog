d693f35bc78a2ec7c1c59b083716b2597a4eb015
rforgione <rforgione@gmail.com>
2016-08-19 00:34:22 -0400

    finish part 3 and begin building social login logic

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from app import db
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin
 
-class User(db.Model):
+lm = LoginManager(app)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     # interestingly, this creates an 'author' property on the 'post'
@@ -11,11 +17,17 @@ class User(db.Model):
     # for the relationship to be complete. That is to say -- there must
     # be a column in the class we're pointing to -- in this case, the Post
     # class, that includes a ForeignKey pointing back to some column in this
-    # table
+    # table. this does not create any fields in this table, but it provides
+    # a convenience method for getting all of the items in the Post table that
+    # are linked back to a given record in the User table
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
